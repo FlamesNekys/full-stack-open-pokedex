87f9cabc770d01fceb07cf5226db7ae83c9a1da4
FlamesNekkys <yodacj21@gmail.com>
2024-03-14 23:18:12 +0300

    broken the test

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -28,7 +28,7 @@ describe('<App />', () => {
         </Router>
       );
     });
-    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledTimes(0);
     expect(axiosMock.get).toHaveBeenCalledWith(
       'https://pokeapi.co/api/v2/pokemon/?limit=50'
     );
